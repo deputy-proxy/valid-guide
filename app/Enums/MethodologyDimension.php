03f3a7e930faea3df8ba557d84b0ba476b05a01b
Proxy for Main <149622634+deputy-proxy@users.noreply.github.com>
2026-09-09 10:40:59 +0300

    Add methodology dimension enum

---
+++ app/Enums/MethodologyDimension.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Enums;
+
+enum MethodologyDimension: string
+{
+    case D1 = 'D1';
+    case D2 = 'D2';
+    case D3 = 'D3';
+    case D4 = 'D4';
+    case D5 = 'D5';
+    case D6 = 'D6';
+    case D7 = 'D7';
+    case D8 = 'D8';
+    case D9 = 'D9';
+    case D10 = 'D10';
+}

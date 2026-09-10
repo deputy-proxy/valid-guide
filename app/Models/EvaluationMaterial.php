cb96957ce992de88649459c33ed3d091ff40d97a
Proxy for Main <149622634+deputy-proxy@users.noreply.github.com>
2026-09-10 20:22:25 +0300

    Normalize material model operator spacing

--- app/Models/EvaluationMaterial.php
+++ app/Models/EvaluationMaterial.php
@@ -62,7 +62,7 @@ class EvaluationMaterial extends Model
             }
 
             $request = EvaluationRequest::query()->find($material->evaluation_request_id);
-            if ($request === null || ! in_array($request->status, self::CREATOR_SUBMISSION_STATUSES, true)) {
+            if ($request === null || !in_array($request->status, self::CREATOR_SUBMISSION_STATUSES, true)) {
                 throw new DomainStateTransitionException('Evaluation materials can only be submitted during the paid intake workflow.');
             }
 
@@ -71,11 +71,11 @@ class EvaluationMaterial extends Model
             }
         });
 
-        static::updating(function (self $material): void {
+        static::updating(function (): void {
             throw new DomainStateTransitionException('Submitted evaluation materials are immutable.');
         });
 
-        static::deleting(function (self $material): void {
+        static::deleting(function (): void {
             throw new DomainStateTransitionException('Submitted evaluation materials cannot be deleted.');
         });
     }

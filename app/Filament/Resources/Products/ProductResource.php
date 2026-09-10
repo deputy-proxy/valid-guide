b494d970c4d85ebbca65fc92dcd107113240f260
Proxy for Main <149622634+deputy-proxy@users.noreply.github.com>
2026-09-10 23:24:20 +0300

    Fix Product navigation icon type

--- app/Filament/Resources/Products/ProductResource.php
+++ app/Filament/Resources/Products/ProductResource.php
@@ -26,6 +26,7 @@ use Filament\Tables\Filters\SelectFilter;
 use Filament\Tables\Table;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Support\Facades\Auth;
+use BackedEnum;
 use UnitEnum;
 
 class ProductResource extends Resource
@@ -34,7 +35,7 @@ class ProductResource extends Resource
 
     protected static string|UnitEnum|null $navigationGroup = 'Creator';
 
-    protected static ?string $navigationIcon = 'heroicon-o-academic-cap';
+    protected static BackedEnum|string|null $navigationIcon = 'heroicon-o-academic-cap';
 
     public static function form(Schema $schema): Schema
     {

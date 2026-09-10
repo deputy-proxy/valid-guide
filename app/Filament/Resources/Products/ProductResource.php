c9685ea0348479bcddd56212276f803c58c7979a
Proxy for Main <149622634+deputy-proxy@users.noreply.github.com>
2026-09-10 23:36:55 +0300

    Restore Pint-compatible Product resource formatting

--- app/Filament/Resources/Products/ProductResource.php
+++ app/Filament/Resources/Products/ProductResource.php
@@ -33,7 +33,9 @@ use UnitEnum;
 class ProductResource extends Resource
 {
     protected static ?string $model = Product::class;
+
     protected static string|UnitEnum|null $navigationGroup = 'Creator';
+
     protected static string|BackedEnum|null $navigationIcon = 'heroicon-o-academic-cap';
 
     public static function form(Schema $schema): Schema

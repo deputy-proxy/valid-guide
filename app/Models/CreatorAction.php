0f8745259ed1bdf305519c54b5f1b0b1e40c48bc
Proxy for Main <149622634+deputy-proxy@users.noreply.github.com>
2026-09-11 21:48:47 +0300

    Fix creator action PHPStan property types

--- app/Models/CreatorAction.php
+++ app/Models/CreatorAction.php
@@ -11,10 +11,14 @@ use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Support\Carbon;
 
 /**
  * @property CreatorActionPriority $priority
  * @property CreatorActionStatus $status
+ * @property int|null $assigned_to
+ * @property Carbon|null $due_at
+ * @property Carbon|null $completed_at
  */
 class CreatorAction extends Model
 {

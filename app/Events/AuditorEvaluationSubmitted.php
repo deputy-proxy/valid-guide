b7e1f6748fecb962dd28cb749deb4294d62328e0
Proxy for Main <149622634+deputy-proxy@users.noreply.github.com>
2026-09-11 16:20:59 +0300

    Add submission notification event

---
+++ app/Events/AuditorEvaluationSubmitted.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Events;
+
+use App\Models\AuditorEvaluation;
+use Illuminate\Contracts\Events\ShouldDispatchAfterCommit;
+
+final readonly class AuditorEvaluationSubmitted implements ShouldDispatchAfterCommit
+{
+    public function __construct(public AuditorEvaluation $auditorEvaluation)
+    {
+    }
+}

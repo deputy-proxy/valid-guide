f9e2c35fb6cfc47df7c720ebc37c7835fcc0d820
Proxy for Main <149622634+deputy-proxy@users.noreply.github.com>
2026-09-11 09:10:54 +0300

    Add public foundation state page

---
+++ resources/views/public/pages/placeholder.blade.php
@@ -0,0 +1,15 @@
+<x-layouts.public
+    :title="$title"
+    :description="$description"
+>
+    <x-public.section>
+        <div class="mx-auto max-w-3xl">
+            <x-public.state
+                :title="$title"
+                :message="$message"
+                :action-url="route('home')"
+                action-label="Back to Valid.guide"
+            />
+        </div>
+    </x-public.section>
+</x-layouts.public>

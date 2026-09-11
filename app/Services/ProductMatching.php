46592f69bffcad0585b35d88232e84b4f0ad8ded
Proxy for Main <149622634+deputy-proxy@users.noreply.github.com>
2026-09-12 01:12:51 +0300

    Fix product match collection type

--- app/Services/ProductMatching.php
+++ app/Services/ProductMatching.php
@@ -11,7 +11,7 @@ use App\Enums\ProductStatus;
 use App\Enums\ProductType;
 use App\Enums\ValidationStatus;
 use App\Models\Product;
-use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Support\Collection;
 
 class ProductMatching
 {

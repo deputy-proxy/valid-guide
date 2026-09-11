db756fda9224ae1d3e0ea59cd923d9a3989fdac0
Proxy for Main <149622634+deputy-proxy@users.noreply.github.com>
2026-09-11 16:25:57 +0300

    Add evaluation decision notification event

--- app/Enums/NotificationEventType.php
+++ app/Enums/NotificationEventType.php
@@ -8,6 +8,7 @@ enum NotificationEventType: string
 {
     case AuditorAssignmentCreated = 'auditor_assignment_created';
     case AuditorEvaluationSubmitted = 'auditor_evaluation_submitted';
+    case EvaluationDecisionRecorded = 'evaluation_decision_recorded';
     case ReportDelivered = 'report_delivered';
     case ClarificationSubmitted = 'clarification_submitted';
     case ClarificationAnswered = 'clarification_answered';

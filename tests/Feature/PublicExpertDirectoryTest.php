1150e484905e566c44bc3ce1f4a238037a40b101
Proxy for Main <149622634+deputy-proxy@users.noreply.github.com>
2026-09-12 23:59:35 +0300

    Add required blank line at EOF for #29

--- tests/Feature/PublicExpertDirectoryTest.php
+++ tests/Feature/PublicExpertDirectoryTest.php
@@ -173,4 +173,4 @@ it('does not discover an Expert after current Board membership is suspended', fu
     $this->get(route('public.experts.show', ['slug' => $publicProfile->slug]))
         ->assertNotFound();
 });
-
+ 

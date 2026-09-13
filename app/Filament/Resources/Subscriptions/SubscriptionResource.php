25c5b98631ed80a51664c4efe6fbc4a258d1165f
Proxy for Main <149622634+deputy-proxy@users.noreply.github.com>
2026-09-13 21:40:00 +0300

    fix(#46): restore subscription query builder import

--- app/Filament/Resources/Subscriptions/SubscriptionResource.php
+++ app/Filament/Resources/Subscriptions/SubscriptionResource.php
@@ -21,6 +21,7 @@ use Filament\Schemas\Schema;
 use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Filters\SelectFilter;
 use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Support\Facades\Auth;
 use UnitEnum;
 use Throwable;

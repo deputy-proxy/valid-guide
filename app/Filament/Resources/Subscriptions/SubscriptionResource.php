385b5547ff271488227b6cde5d02043ca3241e66
Proxy for Main <149622634+deputy-proxy@users.noreply.github.com>
2026-09-13 21:42:59 +0300

    Fix subscription resource import ordering

--- app/Filament/Resources/Subscriptions/SubscriptionResource.php
+++ app/Filament/Resources/Subscriptions/SubscriptionResource.php
@@ -23,8 +23,8 @@ use Filament\Tables\Filters\SelectFilter;
 use Filament\Tables\Table;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Support\Facades\Auth;
-use UnitEnum;
 use Throwable;
+use UnitEnum;
 
 final class SubscriptionResource extends Resource
 {

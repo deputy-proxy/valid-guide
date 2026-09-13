11dfa0bfc0a87a7b7b2f1829f8349105a813d562
Proxy for Main <149622634+deputy-proxy@users.noreply.github.com>
2026-09-13 09:11:20 +0300

    #31 fix PHPStan enum option helper

--- app/Filament/Resources/CommunityContributions/CommunityContributionResource.php
+++ app/Filament/Resources/CommunityContributions/CommunityContributionResource.php
@@ -97,13 +97,7 @@ final class CommunityContributionResource extends Resource
     {
         $options = [];
         foreach ($cases as $case) {
-            if ($case instanceof BackedEnum) {
-                $options[(string) $case->value] = str((string) $case->value)->replace('_', ' ')->title()->toString();
-
-                continue;
-            }
-
-            throw new \LogicException('Community contribution status cases must be backed enums.');
+            $options[(string) $case->value] = str((string) $case->value)->replace('_', ' ')->title()->toString();
         }
 
         return $options;
